64f768625793ac92fa2a311a42c1850a3620e33e
Andrew Knowles <andrew.knowles@control-plane.io>
2021-05-28 16:21:45 +0100

    Basic example

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const country = "uk"
+
+func main() {
+
+	name := "Andrew"
+	from := "hebburn"
+
+	var n int
+	n = 5
+
+	fmt.Printf("my name is %s, and im from %s", name, from)
+	fmt.Printf("By the time %d comes around, we will know how to code in go!!", n)
+}
